cdfeaddc361ec46ebf50ee13a7d03a88d0cecef4
emillaudon <emil.laudn@gmail.com>
2020-05-01 14:22:57 +0200

    Added USD price update with API

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -1,21 +1,94 @@
 package com.example.wallet
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import org.json.JSONObject
+import java.math.RoundingMode
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var balanceInFiatTextView : TextView
+
+    var dm = DataManager
+    //val apiUrl = "https://blockchain.info/tobtc?currency=USD&value=1"
+    val apiUrl = "https://blockchain.info/ticker"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dm = DataManager
+        balanceInFiatTextView = findViewById(R.id.balance_fiat)
 
         val recyclerView = findViewById<RecyclerView>(R.id.transactionsRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         recyclerView.adapter = TransactionsRecyclerAdapter(this, DataManager.transactions)
+
+        stillUnnamed()
     }
+
+    fun stillUnnamed() {
+        AsyncTaskHandleJson().execute(apiUrl)
+    }
+
+    fun calculateBTCToUSD(currentUSDValue: Double, BTC: Double) : Double {
+        println("!!!!! ${currentUSDValue}")
+        println("!!!!! ${BTC}")
+        return currentUSDValue * BTC
+    }
+
+    fun updateValueUSD(currentUSDValue: Double) {
+        val bTCInFiat = calculateBTCToUSD(currentUSDValue, dm.currentBalance)
+        val roundedBalance = roundOffDecimal(bTCInFiat)
+        balanceInFiatTextView.text = "${roundedBalance} USD"
+
+    }
+
+    fun roundOffDecimal(number: Double): Double {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.CEILING
+        return df.format(number).replace(",", ".").toDouble()
+    }
+
+    inner class AsyncTaskHandleJson : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg url: String?): String {
+
+            var text: String
+            val connection = URL(url[0]).openConnection() as HttpURLConnection
+            try{
+                connection.connect()
+                text = connection.inputStream.use { it.reader().use{reader -> reader.readText()} }
+            } finally {
+                connection.disconnect()
+            }
+            return text
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            handleJson(result)
+        }
+    }
+
+    private fun handleJson(jsonString: String?)  {
+        val jsonObject = JSONObject(jsonString)
+        val USDJSON = jsonObject.getJSONObject("USD")
+        val latestUSDValue = USDJSON.getDouble("last")
+
+        println("!!!! ${latestUSDValue}}")
+        var currentWalletBalanceUSD = latestUSDValue * dm.currentBalance
+        println("!!!!! ${currentWalletBalanceUSD}")
+
+        updateValueUSD(latestUSDValue)
+    }
+
+
+
 }
